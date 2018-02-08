98363ee36515fa512911bfdab08256032bbb275f
Jean-Adrien Domage <jeanadrien.domage@epitech.eu>
2018-02-08 16:53:20 +0100

    :construction: update compilation rules

--- node-binding/library.cpp
+++ node-binding/library.cpp
@@ -2,7 +2,8 @@
 // Created by Jean-Adrien on 12/01/18.
 //
 
-#include "plugin.hpp"
+#include <iostream>
+#include "AssimpToJson.hpp"
 
 extern "C" {
 
@@ -10,7 +11,26 @@ extern "C" {
         return  0;
     }
 
-    IModule *load(const char *str) {
-        return new plugin(str);
+    int load(const char *str) {
+        try {
+            std::string path = str;
+            size_t pos = path.find_last_of('.');
+            if (pos == std::string::npos) {
+                return 1;
+            }
+            std::string output;
+            output = (path.substr(0, pos));
+            output.append(".json");
+
+            // Perform the conversion
+            AssimpToJson a2jConverter(path, output);
+            if (!a2jConverter.exec()) {
+                output = a2jConverter.getError();
+            }
+        }
+        catch (std::exception &e) {
+            std::cerr << "[CONVERTER]  " <<  e.what() << std::endl;
+        }
+        return 0;
     }
 }
